77ac2273a80630e5f5dba9d58ed147ef27025fa9
Errors <1075695713@qq.com>
2023-01-25 19:42:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,68 @@
 echo "===================Setup Export========================="
 export ARCH=arm64
-export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
+#export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
 
-echo "=========================Clean========================="
-rm -rf $KERNEL_PATH/out/ *.zip
-make mrproper && git reset --hard HEAD
 
-echo "=========================make========================="
-make O=out alioth_lmperf_defconfig
-make O=out
+KERNEL_DEFCONFIG=alioth_lmperf_defconfig
+
+
+
+
+
+
+
+
+
+
+BUILD_START=$(date +"%s")
+
+blue='\033[1;34m'
+
+yellow='\033[1;33m'
+
+nocol='\033[0m'
+
+# Always do clean build lol
+
+echo -e "$yellow**** Cleaning ****$nocol"
+
+mkdir -p out
+
+make O=out clean
+
+echo -e "$yellow**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****$nocol"
+
+echo -e "$blue***********************************************"
+
+echo "          BUILDING KERNEL          "
+
+echo -e "***********************************************$nocol"
+
+make $KERNEL_DEFCONFIG O=out
+
+make -j$(nproc --all) O=out \
+
+                      ARCH=arm64 \
+
+                      CC=clang \
+
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+find out/arch/arm64/boot/dts/vendor/qcom/ -name '*.dtb' -exec cat {} + >out/arch/arm64/boot/dtb
+
+BUILD_END=$(date +"%s")
+
+DIFF=$(($BUILD_END - $BUILD_START))
+
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+
+
+
+
+
+
